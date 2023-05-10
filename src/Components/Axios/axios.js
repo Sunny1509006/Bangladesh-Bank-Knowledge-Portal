5abf62e0edb320ed50527cea7685c8816716d371
Sunny <sunny1509006@gmail.com>
2023-05-10 12:06:48 +0600

    Footer top portion added

--- src/Components/Axios/axios.js
+++ src/Components/Axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  // baseURL: "http://139.59.60.50:8000",
-  baseURL: "http://139.59.60.50",
+  baseURL: "http://139.59.60.50:8000",
+  // baseURL: "http://139.59.60.50",
   // baseURL: "http://127.0.0.1:8000",
   
 });
--- src/Components/FooterTemp.js
+++ src/Components/FooterTemp.js
@@ -4,6 +4,49 @@ import "./FooterTemp.css"
 const FooterTemp = () => {
     return (
         <div className='FooterTempMain'>
+            <div className='footer_top'>
+                <div style={{
+                    backgroundImage: "url(/images/gcf.png)",
+                    // width: 'calc(100% - 60px)',
+                    width: '100%',
+                    height: '120px',
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    // margin: '0px 30px',
+                }}>
+                    <p>Funded by</p>
+                    <img src="/images/gcf_logo.png" style={{
+                        height: '80px',
+                        width: '140px',
+                    }} />
+                </div>
+                <div style={{
+                    backgroundImage: "url(/images/bb.png)",
+                    // width: 'calc(100% - 60px)',
+                    width: '100%',
+                    height: '120px',
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: '10px',
+                    // margin: '0px 30px',
+                }}>
+                    <p>Planning & Implementation</p>
+                    <img src="/images/BangladeshBank_logo.png" style={{
+                        height: '60px',
+                        width: '60px',
+                    }} />
+                    <img src="/images/BangladeshBank_logo.png" style={{
+                        height: '60px',
+                        width: '60px',
+                    }} />
+                </div>
+            </div>
             <div className='footer_below'>
                 <p className='copyright_p'>Copyright © 2023, <b>Bangladesh Bank</b>, All Right Reserved</p>
                 <div style={{
@@ -11,12 +54,12 @@ const FooterTemp = () => {
 
                 }}>
                     <p className='copyright_p'><b>System Development</b></p>
-                    <img src='/images/Dream71_logo.png' style={{ 
+                    <img src='/images/Dream71_logo.png' style={{
                         width: '80px',
-                         height: '20px',
-                         marginLeft: '8px',
-                         marginTop: '-5px',
-                        }}/>
+                        height: '20px',
+                        marginLeft: '8px',
+                        marginTop: '-5px',
+                    }} />
                 </div>
             </div>
         </div>
